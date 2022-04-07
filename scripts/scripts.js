1eaa244eb462b09555e094cb182906b310fe4be2
Lucas Duarte da Silva <lukazds2016@gmail.com>
2022-04-06 23:33:14 -0300

    Layout Desktop construído

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,24 @@
-prompt("Qual o tipo de Jogo?")
+function numJogos() {
+    let quantjogos = Number(prompt("Com quantas cartas você deseja jogar? (observação digitar um número par de cartas mior que 4 e menor do que 14!)"));
+    while(quantjogos % 2 !== 0) {
+        alert("Por favor, digite um número par de cartas!")
+        quantjogos = Number(prompt("Com quantas cartas você deseja jogar?"));
+    }
+    while(quantjogos < 4) {
+        alert("Por favor, o número digitado é menor que 4!")
+        quantjogos = Number(prompt("Com quantas cartas você deseja jogar?"));
+    }
+    while(quantjogos > 14) {
+        alert("Por favor, o número digitado é maior que 14!")
+        quantjogos = Number(prompt("Com quantas cartas você deseja jogar?"));
+    }
+    return quantjogos; 
+}
+function gerarCartas() {
+    const cartas = document.querySelector(".cartas");
+    const quantidade = numJogos()
+    for(let i = 0; i < quantidade; i ++) {
+        cartas.innerHTML += ` <div class="carta"><img src="/images/front.png"></div>`
+    }
+}
+gerarCartas()
