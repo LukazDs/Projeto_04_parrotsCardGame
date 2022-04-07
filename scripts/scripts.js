afd192b8df35f1ff3bdca48e683a23dc9b866603
Lucas Duarte da Silva <lukazds2016@gmail.com>
2022-04-07 02:13:14 -0300

    Requisito distribuição de cartas atingido

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,26 @@
 function numJogos() {
+
     let quantjogos = Number(prompt("Com quantas cartas você deseja jogar? (observação digitar um número par de cartas mior que 4 e menor do que 14!)"));
-    while(quantjogos % 2 !== 0) {
-        alert("Por favor, digite um número par de cartas!")
+    let minhalista = [];
+
+    while(quantjogos % 2 !== 0 || quantjogos < 4 || quantjogos > 14) {
+        alert("Por favor, algo de errado não está certo!")
         quantjogos = Number(prompt("Com quantas cartas você deseja jogar?"));
+    }  
+    for(let i = 0; i < quantjogos; i++) {
+        minhalista.push(i)
     }
-    while(quantjogos < 4) {
-        alert("Por favor, o número digitado é menor que 4!")
-        quantjogos = Number(prompt("Com quantas cartas você deseja jogar?"));
-    }
-    while(quantjogos > 14) {
-        alert("Por favor, o número digitado é maior que 14!")
-        quantjogos = Number(prompt("Com quantas cartas você deseja jogar?"));
-    }
-    return quantjogos; 
+    return minhalista.sort(comparador) 
+}
+function comparador() { 
+	return Math.random() - 0.5; 
 }
 function gerarCartas() {
     const cartas = document.querySelector(".cartas");
     const quantidade = numJogos()
-    for(let i = 0; i < quantidade; i ++) {
-        cartas.innerHTML += ` <div class="carta"><img src="/images/front.png"></div>`
+    for(let i = 0; i < quantidade.length; i ++) {
+        cartas.innerHTML += ` <div class="carta"><img src="/images/front.png">${quantidade[i]}</div>`
     }
 }
+
 gerarCartas()
