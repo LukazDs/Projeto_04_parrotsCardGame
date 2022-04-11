5e5c1e68d7d9bd554e9187e34a1e074419a7f68a
Lucas Duarte da Silva <lukazds2016@gmail.com>
2022-04-11 14:07:28 -0300

    Bônus Finalizado

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,41 +1,57 @@
+/* Colocar as imagens */
+
 let quantjogos = Number(prompt("Com quantas cartas você deseja jogar? (Observação digitar um número par de cartas maior que 4 e menor do que 14!)"));
-checarJogos()
 const cartas = document.querySelector(".cartas")
-const listaImagens = [
-    "img0.gif",
-    "img1.gif",
-    "img2.gif",
-    "img3.gif",
-    "img4.gif",
-    "img5.gif",
-    "img6.gif",
-]
+let desejar = "sim"
 
-listaImagens.sort(comparador)
+function checarJogos() {
+    while(quantjogos % 2 !== 0 || quantjogos < 4 || quantjogos > 14) {
+        alert("Por favor, algo de errado não está certo!")
+        quantjogos = Number(prompt("Com quantas cartas você deseja jogar?"));
+    }  
+}
 
-const imagensUsadas = listaImagens.slice(0, quantjogos/2)
-const listaImagensUsada = imagensUsadas.concat(imagensUsadas).sort(comparador)
-console.log(listaImagensUsada)
-
-let cartasColocada = "";
-
-for(let i = 0; i < quantjogos; i++){
-    cartasColocada += `
-    <div class="carta" data-carta="${listaImagensUsada[i]}">
-        <img class="face-levantada" src="images/${listaImagensUsada[i]}" />
-        <img class="face-abaixada" src="images/front.png" />
-    </div>
-    `
-};
-
-cartas.innerHTML = cartasColocada;
+function embaralharLista() {
+    const listaImagens = [
+        "img0.gif",
+        "img1.gif",
+        "img2.gif",
+        "img3.gif",
+        "img4.gif",
+        "img5.gif",
+        "img6.gif",
+    ]
     
+    listaImagens.sort(comparador)
+    const imagensUsadas = listaImagens.slice(0, quantjogos/2)
+    return imagensUsadas.concat(imagensUsadas).sort(comparador)
 
+}
+
+function imprimirCartas() {
+    if(desejar === "sim") {lista = embaralharLista()
+    let cartasColocada = "";
+    for(let i = 0; i < quantjogos; i++){
+        cartasColocada += `
+        <div class="carta" data-carta="${lista[i]}">
+            <img class="face-levantada" src="images/${lista[i]}" />
+            <img class="face-abaixada" src="images/front.png" />
+        </div>
+        `
+    }
+    cartas.innerHTML = cartasColocada;}
+}
+
+checarJogos()
+imprimirCartas()
+
+
+/* análise de clique */
 const todasCartas = document.querySelectorAll(".carta")
 let primeiraCarta;
 let segundaCarta;
 let cartaBloqueada = false;
-const listaPares = [];
+let listaPares = [];
 let clique = 0;
 
 function rotacionarCarta() {
@@ -65,7 +81,9 @@ function compararCartas() {
     
     
     !cartasIguais ? desabilitarCartas() : resetarCartas(cartasIguais)
+
     setTimeout(finalizarGame, 1000)
+    
     
 }
 
@@ -86,19 +104,7 @@ function resetarCartas(cartasIguais = false) {
     cartaBloqueada = false;
 }
 
-/* observou um click ativa o rotacionarCarta. ATENÇÃO*/
-/* for(let i = 0; i < todasCartas.length; i++) {
-    todasCartas[i].addEventListener("click", rotacionarCarta)
-}
- */
 todasCartas.forEach(carta => carta.addEventListener("click", rotacionarCarta))
-
-function checarJogos() {
-    while(quantjogos % 2 !== 0 || quantjogos < 4 || quantjogos > 14) {
-        alert("Por favor, algo de errado não está certo!")
-        quantjogos = Number(prompt("Com quantas cartas você deseja jogar?"));
-    }  
-}
 
 function comparador() { 
 	return Math.random() - 0.5; 
@@ -106,7 +112,39 @@ function comparador() {
 
 function finalizarGame() {
     if(listaPares.length === quantjogos / 2) {
-        alert(`Muleke tu é PIKA! Apenas ${clique} jogadas!`)
+        alert(`Você ganhou em ${clique} jogadas! Em ${contador} segundos!`)
+        setTimeout(resetarGame, 1000)    
     }
 }
 
+function resetarGame() {
+    let desejoUsuario = prompt("Você gostaria de jogar novamente?")
+
+    if(desejoUsuario === "Sim") {
+        document.location.reload()
+    } else {
+        cartas.innerHTML = `
+        <p class='creditos'>Autor: LukazDs</p>
+        <img src=images/passarinho.gif />`
+        
+        relogio.classList.add("relogio-block")
+    }
+}
+
+const relogio = document.querySelector(".relogio")
+let contador = 0
+relogio.innerHTML = contador
+
+function tempoJogo() {
+
+    contador += 1;
+    if(listaPares.length < quantjogos / 2) {
+        relogio.innerHTML = contador
+    }
+    else {
+        relogio.innerHTML = ""
+    }
+    
+}
+
+setInterval(tempoJogo, 1000)
